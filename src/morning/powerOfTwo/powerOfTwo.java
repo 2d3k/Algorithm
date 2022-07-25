1f7c5cd290e69062c59c437d19b23ea648c87c83
2D3 <as13337@gmail.com>
2022-07-25 12:37:42 +0900

    2*n

--- src/morning/powerOfTwo/powerOfTwo.java
+++ src/morning/powerOfTwo/powerOfTwo.java
@@ -1,11 +1,21 @@
 /* 문제:  2의 거듭제곱인지 여부 */
 
-public class powerOfTwo {
-    public static void main(String[] args) {
+package com.codestates.coplit;
+import java.util.*;
 
+public class Solution {
+    public boolean powerOfTwo(long num) {
+        //2의 n승이면 true
+        if (num == 1) return true;
+
+        //2의 n승이 아니면 false 리턴
+        if (num % 2 != 0) return false;
+
+        //2의 n승을 구해주는 powered
+        long powered = 2;
+        while (powered < num) {
+            powered = powered * 2;
+        }
+        return powered == num;
     }
 }
-
-/*
-
- */
