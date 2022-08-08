48f2cc7b7e4dc860b8fc6db13561f351f5769474
2D3 <as13337@gmail.com>
2022-08-08 14:52:21 +0900

    BOJ

--- Baekjoon/B2446/Main.java
+++ Baekjoon/B2446/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+
+
         StringBuilder sb = new StringBuilder();
 
 
