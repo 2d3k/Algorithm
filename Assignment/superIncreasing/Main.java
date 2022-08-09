4140c627ea6a48810f3572680970187157d73235
2D3 <as13337@gmail.com>
2022-08-09 10:10:50 +0900

    Assignment

---
+++ Assignment/superIncreasing/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static boolean superIncreasing(int[] arr) {
+        if(arr.length == 0) return false;
+        int sum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] <= sum) {
+                return false;
+            }
+            sum = sum + arr[i];
+        }
+        return true;
+    }
+}
