49e8fc8cf5b3d6ca219d22d338247c9bec5f2a72
2D3 <as13337@gmail.com>
2022-08-09 17:33:16 +0900

    BOJ

--- Baekjoon/B2446/Main.java
+++ Baekjoon/B2446/Main.java
@@ -5,13 +5,29 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-
+        int num = Integer.parseInt(br.readLine());
+        br.close();
 
         StringBuilder sb = new StringBuilder();
 
-
-
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < i; j++) {
+                sb.append(" ");
+            }
+            for (int k = 0; k < 2 * num -(2 * i + 1); k++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        for (int i = 0; i < num - 1; i++) {
+            for (int j = 1; j < num - 1 - i; j++) {
+                sb.append(" ");
+            }
+            for (int k = 0; k < 3 + (2 * i); k++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
         System.out.print(sb);
     }
 }
