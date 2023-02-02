73610f6b1f6a6f4a9b092376eb06bdef681d0352
kim eunjin <102038283+2d3k@users.noreply.github.com>
2023-02-02 14:36:22 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 72 ms, Memory: 11348 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,16 @@
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("       _.-;;-._").append("\n");
+        sb.append("'-..-'|   ||   |").append("\n");
+        sb.append("'-..-'|_.-;;-._|").append("\n");
+        sb.append("'-..-'|   ||   |").append("\n");
+        sb.append("'-..-'|_.-''-._|").append("\n");
+
+        System.out.print(sb);
+    }
+}
