1f6e1e37caa3081bcec72a5258a15d14d8a00c0f
kim eunjin <102038283+2d3k@users.noreply.github.com>
2023-02-02 14:37:26 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 108 ms, Memory: 12884 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        StringBuilder sb = new StringBuilder();
+
+        int[] chess = new int[6];
+        int[] full = new int[]{1, 1, 2, 2, 2, 8};
+
+        for (int i = 0; i < 6; i++) {
+            chess[i] = sc.nextInt();
+        }
+        for (int i = 0; i < full.length; i++) {
+            chess[i] = full[i] - chess[i];
+            System.out.print(chess[i] + " ");
+        }
+    }
+}
