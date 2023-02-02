412a43e8ca23beed46478b5a683faa820997ff4a
kim eunjin <102038283+2d3k@users.noreply.github.com>
2023-02-02 14:29:46 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 132 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= count; i++) {
+            for (int k = count; k > i; k--) {
+                sb.append(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
