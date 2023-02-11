12207e3a55980e4685d7eb9b151e1417680b1abe
kim eunjin <102038283+2d3k@users.noreply.github.com>
2023-02-11 12:06:09 +0900

    Time: 11 ms (40.07%), Space: 41.7 MB (68.83%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str=String.valueOf(x);
+        String reverse= new StringBuilder(str).reverse().toString();
+        
+        if(str.equals(reverse)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
