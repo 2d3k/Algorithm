3e94151fcfb3a339eeffbf2cbaa7df8e01849333
kim eunjin <102038283+2d3k@users.noreply.github.com>
2023-02-11 12:13:03 +0900

    Time: 11 ms (40.07%), Space: 41.8 MB (60.51%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,21 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
+        String str=String.valueOf(x);
+        String reverse= new StringBuilder(str).reverse().toString();
+        
+        if(str.equals(reverse)) {
+            return true;
+        }
+        else {
             return false;
         }
-        
-        int[] num = Stream.of(String.valueOf(x).split("")).mapToInt(Integer::parseInt).toArray();
-        int[] revertNum = new int[num.length];
-        
-        for (int i = 0; i < num.length; i++) {
-            revertNum[i] = num[num.length - 1 - i];
-        }
-        
-        for (int i = 0; i < num.length; i++) {
-            if (num[i] != revertNum[i]) {
-            return false; 
-            }
-        } 
-        return true;
     }
 }
